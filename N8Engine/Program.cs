12e36d4464ff52cce05bbc9c8f92fe8bd1d7fc35
N8Dev <n8dev.cs@gmail.com>
2021-06-18 18:58:17 -0400

    Set up Program.cs with an external terminal.

---
+++ N8Engine/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace N8Engine
+{
+    public static class Program
+    {
+        private static void Main(string[] arguments)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
