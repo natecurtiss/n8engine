edfd4791e5f6d4ffc3491248de6069ecd64f5510
N8Dev <n8dev.cs@gmail.com>
2021-06-18 22:10:48 -0400

    Created MathExtensions.cs and added clamp extension method

---
+++ N8Engine/MathExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace N8Engine
+{
+    public static class MathExtensions
+    {
+        public static void Clamp(this ref float value, in float minimum, in float maximum)
+        {
+            if (value > maximum) 
+                value = maximum;
+            else if (value < maximum) 
+                value = minimum;
+        }
+    }
+}
