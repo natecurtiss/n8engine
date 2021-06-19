51b1558b1c4761184a91de826377c449287f9b30
N8Dev <n8dev.cs@gmail.com>
2021-06-19 15:12:06 -0400

    Added Colors.cs

---
+++ N8Engine/Color.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace N8Engine
+{
+    public struct Color
+    {
+        public float Red { get; set; }
+        public float Green { get; set; }
+        public float Blue { get; set; }
+        public float Alpha { get; set; }
+
+        public Color(in float red, in float green, in float blue, in float alpha = 1f)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+            Alpha = alpha;
+        }
+    }
+}
