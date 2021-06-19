d80d39360298536030975cca21a5e07d65873446
N8Dev <n8dev.cs@gmail.com>
2021-06-18 22:11:06 -0400

    Created Window.cs with basic methods

---
+++ N8Engine/Window.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace N8Engine
+{
+    public static class Window
+    {
+        public static string Title
+        {
+            get => Console.Title;
+            set => Console.Title = value;
+        }
+        public static int Width
+        {
+            get => Console.WindowWidth;
+            set => Console.WindowWidth = value;
+        }
+        public static int Height
+        {
+            get => Console.WindowHeight;
+            set => Console.WindowHeight = value;
+        }
+    }
+}
