e5ac1109d04a7974c2349b740f8cd0e05d62ed4a
N8Dev <n8dev.cs@gmail.com>
2021-06-20 18:55:27 -0400

    Created Unit Tests for GameObject.cs and Component.cs.

---
+++ N8Engine/Source/Components/ComponentTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace N8Engine.Components
+{
+    internal sealed class ComponentTests
+    {
+        [Test]
+        public void TestAddComponent()
+        {
+            GameObject __gameObject = new GameObject("Test AddComponent GameObject");
+            __gameObject.AddComponent(out DummyComponent __dummyComponent);
+            Assert.IsNotNull(__dummyComponent);
+        }
+
+        [Test]
+        public void TestGetComponent()
+        {
+            GameObject __gameObject = new GameObject("Test GetComponent GameObject");
+            Transform __transform = __gameObject.GetComponent<Transform>();
+            Assert.IsNotNull(__transform);
+        }
+        
+        [Test]
+        public void TestAddAndGetComponent()
+        {
+            GameObject __gameObject = new GameObject("Test GetComponent GameObject");
+            __gameObject.AddComponent<DummyComponent>();
+            Assert.IsNotNull(__gameObject.GetComponent<DummyComponent>());
+        }
+    }
+}
