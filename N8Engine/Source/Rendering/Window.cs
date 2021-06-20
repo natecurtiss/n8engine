834d6a1b5317422de97f182879681f373e19cd7c
N8Dev <n8dev.cs@gmail.com>
2021-06-20 17:13:53 -0400

    Added summaries to Window.cs.

--- N8Engine/Source/Rendering/Window.cs
+++ N8Engine/Source/Rendering/Window.cs
@@ -1,20 +1,33 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 
 namespace N8Engine.Rendering
 {
+    /// <summary>
+    /// The Console Window used to display the game.
+    /// </summary>
     public static class Window
     {
+        /// <summary>
+        /// The title of the window.
+        /// </summary>
         public static string Title
         {
             get => Console.Title;
             set => Console.Title = value;
         }
+        
+        /// <summary>
+        /// The width of the window.
+        /// </summary>
         public static int Width
         {
             get => Console.WindowWidth;
             set => Console.WindowWidth = value;
         }
+        
+        /// <summary>
+        /// The height of the window.
+        /// </summary>
         public static int Height
         {
             get => Console.WindowHeight;
