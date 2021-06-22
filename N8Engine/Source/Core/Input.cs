01b4f2efb130799561dea522a1fdad6bb612ff15
N8Dev <n8dev.cs@gmail.com>
2021-06-21 22:04:10 -0400

    Added basic input.

---
+++ N8Engine/Source/Core/Input.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using N8Engine.Mathematics;
+
+namespace N8Engine.Core
+{
+    public static class Input
+    {
+        
+    }
+}
---
+++ N8Engine/Source/Objects/DummyInputGameObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using N8Engine.Rendering;
+
+namespace N8Engine.Objects
+{
+    public sealed class DummyInputGameObject : GameObject
+    {
+        protected override Sprite RenderSprite() => null;
+
+        protected override void OnUpdate(in float deltaTime)
+        {
+            Console.WriteLine(Console.ReadKey().Key);
+        }
+    }
+}
