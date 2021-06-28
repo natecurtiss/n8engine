998973e52cf8520919ff32c8338a12c72f48c9ef
N8Dev <n8dev.cs@gmail.com>
2021-06-27 21:50:21 -0400

    Updated summaries.

--- N8Engine/Source/Mathematics/Vector2.cs
+++ N8Engine/Source/Mathematics/Vector2.cs
@@ -14,17 +14,17 @@ namespace N8Engine.Mathematics
         #region Equality Overload Methods
 
         /// <summary>
-        /// Returns true if the vector is equal to the vector passed in.
+        /// Returns true if the <see cref="Vector2"/> is equal to the <see cref="Vector2"/> passed in.
         /// </summary>
-        /// <param name="vector"> The vector to compare to. </param>
-        /// <returns> True if the vectors are equal. </returns>
+        /// <param name="vector"/>"> The <see cref="Vector2"/> to compare to. </param>
+        /// <returns> True if the <see cref="Vector2"/>s are equal. </returns>
         public bool Equals(in Vector2 vector) => this == vector;
 
         /// <summary>
-        /// Returns true if the vector is equal to the object passed in.
+        /// Returns true if the <see cref="Vector2"/> is equal to the object passed in.
         /// </summary>
         /// <param name="obj"> The object to compare to. </param>
-        /// <returns> True if the vector is equal to the object. </returns>
+        /// <returns> True if the <see cref="Vector2"/> is equal to the object. </returns>
         public override bool Equals(object obj) => obj is Vector2 __other && this == __other;
 
         public override int GetHashCode() => base.GetHashCode();
@@ -34,84 +34,84 @@ namespace N8Engine.Mathematics
         #region Operator Overloads
 
         /// <summary>
-        /// Returns true if the vectors are equal
+        /// Returns true if the <see cref="Vector2"/>s are equal
         /// </summary>
-        /// <param name="first"> The first vector. </param>
-        /// <param name="second"> The second vector. </param>
-        /// <returns> True if the vectors' X and Y values are equal. </returns>
+        /// <param name="first"> The first <see cref="Vector2"/>. </param>
+        /// <param name="second"> The second <see cref="Vector2"/>. </param>
+        /// <returns> True if the <see cref="Vector2"/>s' X and Y values are equal. </returns>
         public static bool operator ==(in Vector2 first, in Vector2 second) => first.X == second.X && first.Y == second.Y;
 
         /// <summary>
-        /// Returns true if the vectors are not equal.
+        /// Returns true if the <see cref="Vector2"/>s are not equal.
         /// </summary>
-        /// <param name="first"> The first vector. </param>
-        /// <param name="second"> The second vector. </param>
-        /// <returns> True if the vectors' X and Y values are not equal. </returns>
+        /// <param name="first"> The first <see cref="Vector2"/>. </param>
+        /// <param name="second"> The second <see cref="Vector2"/>. </param>
+        /// <returns> True if the <see cref="Vector2"/>s' X and Y values are not equal. </returns>
         public static bool operator !=(in Vector2 first, in Vector2 second) => !(first == second);
         
         /// <summary>
-        /// Adds two vectors.
+        /// Adds two <see cref="Vector2"/>s.
         /// </summary>
-        /// <param name="first"> The first vector. </param>
-        /// <param name="second"> The second vector. </param>
+        /// <param name="first"> The first <see cref="Vector2"/>. </param>
+        /// <param name="second"> The second <see cref="Vector2"/>. </param>
         /// <returns> The result of the addition. </returns>
         public static Vector2 operator +(in Vector2 first, in Vector2 second) => new(first.X + second.X, first.Y + second.Y);
         
         /// <summary>
-        /// Subtracts two vectors.
+        /// Subtracts two <see cref="Vector2"/>s.
         /// </summary>
-        /// <param name="first"> The vector to subtract from. </param>
-        /// <param name="second"> The vector to subtract by. </param>
+        /// <param name="first"> The <see cref="Vector2"/> to subtract from. </param>
+        /// <param name="second"> The <see cref="Vector2"/> to subtract by. </param>
         /// <returns> The result of the subtraction. </returns>
         public static Vector2 operator -(in Vector2 first, in Vector2 second) => new(first.X - second.X, first.Y - second.Y);
         
         /// <summary>
-        /// Multiplies a vector by a float.
+        /// Multiplies a <see cref="Vector2"/> by a float.
         /// </summary>
-        /// <param name="vector"> A vector. </param>
+        /// <param name="<see cref="Vector2"/>"> A <see cref="Vector2"/>. </param>
         /// <param name="multiplier"> The value to multiply by. </param>
         /// <returns> The result of the multiplication. </returns>
         public static Vector2 operator *(in Vector2 vector, in float multiplier) => new(vector.X * multiplier, vector.Y * multiplier);
         
         /// <summary>
-        /// Multiplies a float by a vector.
+        /// Multiplies a float by a <see cref="Vector2"/>.
         /// </summary>
         /// <param name="multiplier"> The value to multiply by. </param>
-        /// <param name="vector"> A vector. </param>
+        /// <param name="<see cref="Vector2"/>"> A <see cref="Vector2"/>. </param>
         /// <returns> The result of the multiplication. </returns>
         public static Vector2 operator *(in float multiplier, in Vector2 vector) => new(vector.X * multiplier, vector.Y * multiplier);
         
         /// <summary>
-        /// Multiplies two vectors by returning the product of each pair of elements.
+        /// Multiplies two <see cref="Vector2"/>s by returning the product of each pair of elements.
         /// </summary>
-        /// <param name="first"> The first vector. </param>
-        /// <param name="second"> The second vector. </param>
-        /// <returns> The product of the vectors' pairs of elements; (2, 1) * (3, 2) = (6, 2). </returns>
+        /// <param name="first"> The first <see cref="Vector2"/>. </param>
+        /// <param name="second"> The second <see cref="Vector2"/>. </param>
+        /// <returns> The product of the <see cref="Vector2"/>s' pairs of elements; (2, 1) * (3, 2) = (6, 2). </returns>
         public static Vector2 operator *(in Vector2 first, in Vector2 second) => new(first.X * second.X, first.Y * second.Y);
         
         /// <summary>
-        /// Divides a vector by a float.
+        /// Divides a <see cref="Vector2"/> by a float.
         /// </summary>
-        /// <param name="vector"> A vector. </param>
+        /// <param name="vector"> A <see cref="Vector2"/>. </param>
         /// <param name="divisor"> The value to divide by. </param>
         /// <returns> The result of the division. </returns>
         public static Vector2 operator /(in Vector2 vector, in float divisor) => new(vector.X / divisor, vector.Y / divisor);
         
         /// <summary>
-        /// Divides two vectors by returning the quotient of each pair of elements.
+        /// Divides two <see cref="Vector2"/>s by returning the quotient of each pair of elements.
         /// </summary>
-        /// <param name="first"> The first vector. </param>
-        /// <param name="second"> The second vector. </param>
-        /// <returns> The quotient of the vectors' pairs of elements; (6, 8) / (3, 2) = (2, 4). </returns>
+        /// <param name="first"> The first <see cref="Vector2"/>. </param>
+        /// <param name="second"> The second <see cref="Vector2"/>. </param>
+        /// <returns> The quotient of the <see cref="Vector2"/>s' pairs of elements; (6, 8) / (3, 2) = (2, 4). </returns>
         public static Vector2 operator /(in Vector2 first, in Vector2 second) => new(first.X / second.X, first.Y / second.Y);
         
         #endregion
         
         #region Base Values
         
-        /// <summary> The first value of the vector. </summary>
+        /// <summary> The first value of the <see cref="Vector2"/>. </summary>
         public float X { get; set; }
-        /// <summary> The second value of the vector. </summary>
+        /// <summary> The second value of the <see cref="Vector2"/>. </summary>
         public float Y { get; set; }
         
         #endregion
@@ -119,52 +119,52 @@ namespace N8Engine.Mathematics
         #region Shortcuts
         
         /// <summary>
-        /// A vector of (0, 0).
+        /// A <see cref="Vector2"/> of (0, 0).
         /// </summary>
         public static Vector2 Zero => new();
         
         /// <summary>
-        /// A vector of (1, 1).
+        /// A <see cref="Vector2"/> of (1, 1).
         /// </summary>
         public static Vector2 One => new(1f);
         
         /// <summary>
-        /// A vector of (0, 1).
+        /// A <see cref="Vector2"/> of (0, 1).
         /// </summary>
         public static Vector2 Up => new(0f, 1f);
         
         /// <summary>
-        /// A vector of (0, -1).
+        /// A <see cref="Vector2"/> of (0, -1).
         /// </summary>
         public static Vector2 Down => new(0f, -1f);
         
         /// <summary>
-        /// A vector of (1, 0).
+        /// A <see cref="Vector2"/> of (1, 0).
         /// </summary>
         public static Vector2 Right => new(1f, 0f);
         
         /// <summary>
-        /// A vector of (-1, 0).
+        /// A <see cref="Vector2"/> of (-1, 0).
         /// </summary>
         public static Vector2 Left => new (-1f, 0f);
         
         /// <summary>
-        /// A vector of (1, 1).
+        /// A <see cref="Vector2"/> of (1, 1).
         /// </summary>
         public static Vector2 UpRight => new(1f);
         
         /// <summary>
-        /// A vector of (-1, 1).
+        /// A <see cref="Vector2"/> of (-1, 1).
         /// </summary>
         public static Vector2 UpLeft => new(-1f, 1f);
         
         /// <summary>
-        /// A vector of (1, -1).
+        /// A <see cref="Vector2"/> of (1, -1).
         /// </summary>
         public static Vector2 DownRight => new(1f, -1f);
         
         /// <summary>
-        /// A vector of (-1, -1).
+        /// A <see cref="Vector2"/> of (-1, -1).
         /// </summary>
         public static Vector2 DownLeft => new(-1f);
 
@@ -173,7 +173,7 @@ namespace N8Engine.Mathematics
         #region Properties
         
         /// <summary>
-        /// The magnitude/length of the vector.
+        /// The magnitude/length of the <see cref="Vector2"/>.
         /// </summary>
         public float Magnitude => this.AsSystemVector2().Length();
         
@@ -202,7 +202,7 @@ namespace N8Engine.Mathematics
         #region Constructors
         
         /// <summary>
-        /// Creates a vector with an equal X and Y
+        /// Creates a <see cref="Vector2"/> with an equal X and Y
         /// </summary>
         /// <param name="both"> The value of both X and Y </param>
         public Vector2(in float both)
@@ -212,7 +212,7 @@ namespace N8Engine.Mathematics
         }
 
         /// <summary>
-        /// Created a Vector with an X and Y
+        /// Created a <see cref="Vector2"/> with an X and Y
         /// </summary>
         /// <param name="x"> The X value </param>
         /// <param name="y"> The Y value </param>
@@ -227,25 +227,25 @@ namespace N8Engine.Mathematics
         #region Instance Methods
         
         /// <summary>
-        /// Adds a vector to the current vector.
+        /// Adds a <see cref="Vector2"/> to the current vector.
         /// </summary>
         /// <param name="addend"> The vector to add by. </param>
         public void Add(in Vector2 addend) => this = this + addend;
 
         /// <summary>
-        /// Subtracts a vector from the current vector.
+        /// Subtracts a <see cref="Vector2"/> from the current vector.
         /// </summary>
         /// <param name="difference"> The vector to subtract by. </param>
         public void Subtract(in Vector2 difference) => this = this - difference;
         
         /// <summary>
-        /// Multiplies the current vector by a float.
+        /// Multiplies the current <see cref="Vector2"/> by a float.
         /// </summary>
         /// <param name="multiplier"> The float to multiply by. </param>
         public void Multiply(in float multiplier) => this = this * multiplier;
 
         /// <summary>
-        /// Multiplies the current vector by another vector.
+        /// Multiplies the current <see cref="Vector2"/> by another <see cref="Vector2"/>.
         /// </summary>
         /// <param name="multiplier"> The vector to multiply by. </param>
         public void Multiply(in Vector2 multiplier) => this = this * multiplier;
@@ -382,53 +382,53 @@ namespace N8Engine.Mathematics
         /// <summary>
         /// Returns the squared Euclidean distance between two vectors using the Pythagorean Theorem.
         /// </summary>
-        /// <param name="first"> The first vector. </param>
-        /// <param name="second"> The second vector. </param>
-        /// <returns> The squared Euclidean distance between the two vectors. </returns>
+        /// <param name="first"> The first <see cref="Vector2"/>. </param>
+        /// <param name="second"> The second <see cref="Vector2"/>. </param>
+        /// <returns> The squared Euclidean distance between the two <see cref="Vector2"/>s. </returns>
         public static float DistanceSquared(in Vector2 first, in Vector2 second) => first.DistanceToSquared(second);
         
         /// <summary>
-        /// Returns the dot product between two vectors.
+        /// Returns the dot product between two <see cref="Vector2"/>s.
         /// </summary>
-        /// <param name="first"> The first vector. </param>
-        /// <param name="second"> The second vector. </param>
-        /// <returns> The dot product between the two vectors. </returns>
+        /// <param name="first"> The first <see cref="Vector2"/>. </param>
+        /// <param name="second"> The second <see cref="Vector2"/>. </param>
+        /// <returns> The dot product between the two <see cref="Vector2"/>s. </returns>
         public static float DotProduct(in Vector2 first, in Vector2 second) => SystemVector2.Dot(first.AsSystemVector2(), second.AsSystemVector2());
         
         /// <summary>
-        /// Returns a linear interpolation between two vectors by an amount clamped between 0 and 1.
+        /// Returns a linear interpolation between two <see cref="Vector2"/>s by an amount clamped between 0 and 1.
         /// </summary>
-        /// <param name="vector"> The vector to interpolate. </param>
-        /// <param name="target"> The target vector to interpolate to. </param>
+        /// <param name="vector"> The <see cref="Vector2"/> to interpolate. </param>
+        /// <param name="target"> The target <see cref="Vector2"/> to interpolate to. </param>
         /// <param name="amount"> The amount to interpolate by, clamped between 0 and 1. </param>
-        /// <returns> The first vector interpolated by amount to the target vector. </returns>
+        /// <returns> The first <see cref="Vector2"/> interpolated by amount to the target <see cref="Vector2"/>. </returns>
         public static Vector2 Lerp(in Vector2 vector, in Vector2 target, in float amount) =>
             SystemVector2.Lerp(vector.AsSystemVector2(), target.AsSystemVector2(), amount.Clamped01()).AsVector2();
 
         /// <summary>
-        /// Returns a vector who's elements are the minimum of each of the pairs of elements in the two vectors; (0,5) and (1, 3) returns (0, 3).
+        /// Returns a <see cref="Vector2"/> who's elements are the minimum of each of the pairs of elements in the two <see cref="Vector2"/>s; (0,5) and (1, 3) returns (0, 3).
         /// </summary>
-        /// <param name="first"> The first vector. </param>
-        /// <param name="second"> The second vector. </param>
-        /// <returns> a vector who's elements are the minimum of each of the pairs of elements in the two vectors; (0,5) and (1, 3) returns (0, 3). </returns>
+        /// <param name="first"> The first <see cref="Vector2"/>. </param>
+        /// <param name="second"> The second <see cref="Vector2"/>. </param>
+        /// <returns> A <see cref="Vector2"/> who's elements are the minimum of each of the pairs of elements in the two <see cref="Vector2"/>s; (0,5) and (1, 3) returns (0, 3). </returns>
         public static Vector2 Minimum(Vector2 first, in Vector2 second) =>
             new(Math.Minimum(first.X, second.X), Math.Minimum(first.Y, second.Y));
         
         /// <summary>
-        /// Returns a vector who's elements are the maximum of each of the pairs of elements in the two vectors; (0,5) and (1, 3) returns (1, 5).
+        /// Returns a <see cref="Vector2"/> who's elements are the maximum of each of the pairs of elements in the two <see cref="Vector2"/>s; (0,5) and (1, 3) returns (1, 5).
         /// </summary>
-        /// <param name="first"> The first vector. </param>
-        /// <param name="second"> The second vector. </param>
-        /// <returns> a vector who's elements are the maximum of each of the pairs of elements in the two vectors; (0,5) and (1, 3) returns (1, 5). </returns>
+        /// <param name="first"> The first <see cref="Vector2"/>. </param>
+        /// <param name="second"> The second <see cref="Vector2"/>. </param>
+        /// <returns> A <see cref="Vector2"/> who's elements are the maximum of each of the pairs of elements in the two <see cref="Vector2"/>s; (0,5) and (1, 3) returns (1, 5). </returns>
         public static Vector2 Maximum(in Vector2 first, in Vector2 second) =>
             new(Math.Maximum(first.X, second.X), Math.Minimum(first.Y, second.Y));
 
         /// <summary>
-        /// Returns a reflection of the current vector off of a surface's normal vector.
+        /// Returns a reflection of the current <see cref="Vector2"/> off of a surface's normal <see cref="Vector2"/>.
         /// </summary>
-        /// <param name="vector"> The vector to reflect. </param>
-        /// <param name="normal"> The normal of the surface to reflect off of. </param>
-        /// <returns> a reflection of the current v off of a surface's normal vector. </returns>
+        /// <param name="vector"> The <see cref="Vector2"/> to reflect. </param>
+        /// <param name="normal"> The normal <see cref="Vector2"/> of the surface to reflect off of. </param>
+        /// <returns> A reflection of the current <see cref="Vector2"/> off of a surface's normal <see cref="Vector2"/>. </returns>
         public static Vector2 Reflect(in Vector2 vector, in Vector2 normal) =>
             SystemVector2.Reflect(vector.AsSystemVector2(), normal.AsSystemVector2()).AsVector2();
 
@@ -436,7 +436,7 @@ namespace N8Engine.Mathematics
     }
 
     /// <summary>
-    /// A static class with extension methods for converting to and from System.Numerics.Vector2 and N8Engine.Mathematics.Vector2.
+    /// A static class with extension methods for converting to and from System.Numerics.Vector2 and <see cref="Vector2"/>.
     /// </summary>
     internal static class InternalVector2Extensions
     {
