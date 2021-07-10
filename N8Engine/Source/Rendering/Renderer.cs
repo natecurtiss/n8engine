c03afdb565b7dc627324d5fe3528c5a823e1997f
N8Dev <n8dev.cs@gmail.com>
2021-07-10 11:44:03 -0400

    Ok what somehow this works.

--- N8Engine/Source/Rendering/Renderer.cs
+++ N8Engine/Source/Rendering/Renderer.cs
@@ -7,6 +7,8 @@ namespace N8Engine.Rendering
 { 
     internal static class Renderer
     {
+        public const int PIXEL_SIZE = 2;
+        
         private static readonly Dictionary<Vector2, Pixel?> _world = new();
         private static Dictionary<Vector2, Pixel?> _worldLastFrame = new();
 
@@ -18,13 +20,12 @@ namespace N8Engine.Rendering
 
         private static void OnPreRender()
         {
-            _worldLastFrame = _world;
+            _worldLastFrame = new Dictionary<Vector2, Pixel?>(_world);
             _world.Clear();
         }
 
         public static void Render(in GameObject gameObject)
         {
-            ClearWindow();
             Sprite __sprite = gameObject.Sprite;
             Vector2 __position = gameObject.Position;
             foreach (Pixel __pixel in __sprite.Pixels)
@@ -42,7 +43,6 @@ namespace N8Engine.Rendering
         
         private static void OnPostRender()
         {
-            Console.ResetColor();
             foreach (Vector2 __position in _world.Keys)
             {
                 if (!_world[__position].HasValue) continue;
@@ -53,19 +53,25 @@ namespace N8Engine.Rendering
                 Console.SetCursorPosition(__position.X.Rounded(), __position.Y.Rounded());
                 Console.ForegroundColor = __pixelToRender.ForegroundColor;
                 Console.BackgroundColor = __pixelToRender.BackgroundColor;
-                Console.Write('▒');
+                for (int __i = 0; __i < PIXEL_SIZE; __i++) Console.Write('▒');
             }
             Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.Black;
+            // ClearWindow();
         }
 
         private static void ClearWindow()
         {
-            foreach (Vector2 __position in _worldLastFrame.Keys.Where(position => _worldLastFrame[position].HasValue).Where(position => !(position.X < 0) && !(position.Y < 0)))
+            foreach (Vector2 __pixelPositionLastFrame in _worldLastFrame.Keys)
             {
-                Console.SetCursorPosition(__position.X.Rounded(), __position.Y.Rounded());
-                Console.Write(' ');
+                if (__pixelPositionLastFrame.X < 0 || __pixelPositionLastFrame.Y < 0) continue;
+                if (!_world.ContainsKey(__pixelPositionLastFrame) || !_world[__pixelPositionLastFrame].HasValue)
+                {
+                    Console.SetCursorPosition(__pixelPositionLastFrame.X.Rounded(), __pixelPositionLastFrame.Y.Rounded());
+                    for (int __i = 0; __i < PIXEL_SIZE; __i++) Console.Write(" ");
+                }
             }
+
         }
     }
 }
