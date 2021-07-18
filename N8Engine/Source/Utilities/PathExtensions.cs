14b9944c709c951027ce2d0cffdee49011b512ec
Walter Hulsebos <walterhulsebos@outlook.com>
2021-07-18 16:49:20 +0200

    NEW: PathExtensions (static class)

---
+++ N8Engine/Source/Utilities/PathExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace N8Engine.Utilities
+{
+	public static class PathExtensions
+	{
+		public static String Combine(this String input, in String single)
+			=> Path.Combine(input, single);
+
+		public static String Combine(this String input, params String[] multiple)
+		{
+			String[] __strings = new String[multiple.Length + 1];
+
+			__strings[0] = input;
+			for (Int32 __index = 0; __index < multiple.Length; __index++)
+			{
+				__strings[__index + 1] = multiple[__index];
+			}
+
+			return Path.Combine(__strings);
+		}
+	}
+}
