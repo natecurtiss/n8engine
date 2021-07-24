8bae8a6079e607245bc905585b7ae38cff7f5cf1
N8Dev <n8dev.cs@gmail.com>
2021-07-24 12:13:27 -0400

    Finished Debug Console application class.

--- DebugConsole/-Source-/Application.cs
+++ DebugConsole/-Source-/Application.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using DebugConsole;
 using Shared;
 
@@ -9,10 +10,22 @@ namespace DebugConsole
     internal static class Application
     {
         private static string _path;
+        private static int _numberOfWrittenLinesInConsole;
         
         public static void Initialize()
         {
+            Console.Title = "N8Engine Debug Console";
             _path = PathExtensions.PathToRootFolder + "/.logs";
+            while (true)
+            {
+                var fileLines = File.ReadAllLines(_path);
+                if (fileLines.Length != _numberOfWrittenLinesInConsole)
+                    for (var line = _numberOfWrittenLinesInConsole - 1; line < fileLines.Length; line++)
+                    {
+                        Console.WriteLine(line);
+                        _numberOfWrittenLinesInConsole++;
+                    }
+            }
         }
     }
 }
