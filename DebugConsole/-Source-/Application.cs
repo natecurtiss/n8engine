71537cf6e5112968beea6a6683e8981c6f93c9ce
N8Dev <n8dev.cs@gmail.com>
2021-07-24 12:01:44 -0400

    Added DebugConsole and Shared projects to VCS.

---
+++ DebugConsole/-Source-/Application.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DebugConsole;
+using Shared;
+
+Application.Initialize();
+
+namespace DebugConsole
+{
+    internal static class Application
+    {
+        private static string _path;
+        
+        public static void Initialize()
+        {
+            _path = PathExtensions.PathToRootFolder + "/.logs";
+        }
+    }
+}
