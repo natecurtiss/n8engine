da752382abdfdcb468644f69eb285282d8eedfbb
N8Dev <n8dev.cs@gmail.com>
2021-09-02 12:04:19 -0400

    Added pivot enum.

---
+++ N8Engine/-Source-/Mathematics/Pivot.cs
@@ -0,0 +1,15 @@
+namespace N8Engine.Mathematics
+{
+    public enum Pivot
+    {
+        Center,
+        Top,
+        Bottom,
+        Left,
+        Right,
+        TopLeft,
+        TopRight,
+        BottomLeft,
+        BottomRight
+    }
+}
