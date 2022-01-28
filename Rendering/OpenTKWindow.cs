bef855285c8a1822ed51c087c51bcaaf09ba6fe5
N8Dev <n8dev.cs@gmail.com>
2022-01-28 17:14:50 -0500

    Taking a break for now

--- Rendering/OpenTKWindow.cs
+++ Rendering/OpenTKWindow.cs
@@ -1,17 +1,26 @@
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 
 namespace N8Engine.Rendering;
 
 public sealed class OpenTKWindow : GameWindow, Window
 {
-    public OpenTKWindow(int width, int height, string title) : base(GameWindowSettings.Default, NativeWindowSettings.Default)
+    public OpenTKWindow(int width, int height, string title) : base(GameWindowSettings.Default, NativeWindowSettings.Default) { }
+
+    protected override void OnLoad()
     {
+        base.OnLoad();
         CenterWindow();
-        Size = new(width, height);
-        Title = title;
-        Run();
+        Size = new(1920, 1080);
     }
-    
-    void Module.Initialize() { }
+
+    protected override void OnResize(ResizeEventArgs args)
+    {
+        base.OnResize(args);
+        GL.Viewport(0, 0, Size.X, Size.Y);
+    }
+
+    void Module.Initialize() => Run();
     void Module.Update(Frame frame) { }
 }
