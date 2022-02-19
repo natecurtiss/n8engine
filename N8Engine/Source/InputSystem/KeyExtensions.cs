9e20ad59f0642ab126eac2031e7b980818aac125
natecurtiss <n8dev.cs@gmail.com>
2022-02-19 16:36:13 -0500

    Add basic InputSystem and Windowing

---
+++ N8Engine/Source/InputSystem/KeyExtensions.cs
@@ -0,0 +1,8 @@
+﻿using GLKey = Silk.NET.Input.Key;
+
+namespace N8Engine.InputSystem;
+
+static class KeyExtensions
+{
+    public static Key AsKey(this GLKey key) => (Key) (int) key;
+}
--- N8Engine/Source/Windowing/WindowOptions.cs
+++ N8Engine/Source/Windowing/WindowOptions.cs
@@ -1,15 +1,31 @@
 ﻿using N8Engine.Mathematics;
+using Silk.NET.Windowing;
+using GLWindowOptions = Silk.NET.Windowing.WindowOptions;
 
 namespace N8Engine.Windowing;
 
-public readonly struct WindowOptions
+readonly struct WindowOptions
 {
-    internal readonly UIntVector Size;
-    internal readonly int FramesPerSecond;
-
-    public WindowOptions(UIntVector size, int fps)
+    public static implicit operator GLWindowOptions(WindowOptions options)
     {
-        Size = size;
-        FramesPerSecond = fps;
+        var result = GLWindowOptions.Default;
+        result.Title = options._title;
+        result.Size = new((int) options._size.X, (int) options._size.Y);
+        result.FramesPerSecond = options._fps;
+        result.WindowState = options._isFullscreen ? WindowState.Fullscreen : WindowState.Normal;
+        return result;
+    }
+
+    readonly string _title;
+    readonly UIntVector _size;
+    readonly int _fps;
+    readonly bool _isFullscreen;
+
+    public WindowOptions(string title, UIntVector size, int fps, bool isFullscreen)
+    {
+        _title = title;
+        _size = size;
+        _fps = fps;
+        _isFullscreen = isFullscreen;
     }
 }
