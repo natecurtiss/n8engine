b677e2638ba825cb93c9deec4915786839624ed8
natecurtiss <n8dev.cs@gmail.com>
2022-03-10 18:04:16 -0500

    Add Texture to SpriteRenderer but it doesn't work lmao

--- N8Engine.Core/Source/Rendering/SpriteRenderer.cs
+++ N8Engine.Core/Source/Rendering/SpriteRenderer.cs
@@ -11,7 +11,7 @@ public sealed class SpriteRenderer : SceneModule
          0.5f,  0.5f, 0.0f, 1f, 0f,
          0.5f, -0.5f, 0.0f, 1f, 1f,
         -0.5f, -0.5f, 0.0f, 0f, 1f,
-        -0.5f,  0.5f, 0.5f, 0f, 0f
+        -0.5f,  0.5f, 0.0f, 0f, 0f
     };
     readonly uint[] _indices =
     {
@@ -20,11 +20,11 @@ public sealed class SpriteRenderer : SceneModule
     };
 
     GL _gl;
-
     BufferObject<float> _vbo;
     BufferObject<uint> _ebo;
     VertexArrayObject<float, uint> _vao;
     Shader _shader;
+    Texture _texture;
 
     void SceneModule.OnSceneLoad(Scene scene)
     {
@@ -37,7 +37,8 @@ public sealed class SpriteRenderer : SceneModule
         _vao.VertexAttributePointer(0, 3, VertexAttribPointerType.Float, 7, 0);
         _vao.VertexAttributePointer(1, 4, VertexAttribPointerType.Float, 7, 3);
 
-        _shader = new(_gl, "../../../../Assets/Shaders/colors.vert", "../../../../Assets/Shaders/colors.frag");
+        _shader = new(_gl, "../../../../Assets/Shaders/sprite.vert", "../../../../Assets/Shaders/sprite.frag");
+        _texture = new(_gl, "../../../../Assets/Textures/n8dev.png");
     }
 
     void SceneModule.OnSceneUpdate() { }
@@ -48,6 +49,8 @@ public sealed class SpriteRenderer : SceneModule
         _gl.Clear(ClearBufferMask.ColorBufferBit);
         _vao.Bind();
         _shader.Use();
+        _texture.Bind();
+        _shader.SetUniform("uTexture0", 0);
         _gl.DrawElements(PrimitiveType.Triangles, (uint) _indices.Length, DrawElementsType.UnsignedInt, null);
     }
 
@@ -57,5 +60,6 @@ public sealed class SpriteRenderer : SceneModule
         _ebo.Dispose();
         _vao.Dispose();
         _shader.Dispose();
+        _texture.Dispose();
     }
 }
