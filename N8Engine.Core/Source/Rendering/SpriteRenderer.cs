9a7cf2dcc13a2209c445767ac1d57a54b7ea2a27
natecurtiss <n8dev.cs@gmail.com>
2022-03-10 17:14:40 -0500

    Change color of quad

--- N8Engine.Core/Source/Rendering/SpriteRenderer.cs
+++ N8Engine.Core/Source/Rendering/SpriteRenderer.cs
@@ -19,7 +19,7 @@ public sealed class SpriteRenderer : SceneModule
     layout (location = 0) out vec4 FragColor;
     void main()
     {
-        FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+        FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
     }
     ";
     
