3641b64f25b26181e6d8cfb6ad9dca9354155219
N8Dev <n8dev.cs@gmail.com>
2022-03-11 10:11:32 -0500

    Add Transform ViewMatrix

--- N8Engine.Core/Source/Core/Transform.cs
+++ N8Engine.Core/Source/Core/Transform.cs
@@ -1,4 +1,6 @@
 ﻿using System.Numerics;
+using N8Engine.Utilities;
+using static System.Numerics.Matrix4x4;
 
 namespace N8Engine;
 
@@ -12,4 +14,6 @@ public sealed class Transform : Component
     public Transform(Vector2 position) => Position = position;
     public Transform(Vector2 position, Vector2 scale) : this(position) => Scale = scale;
     public Transform(Vector2 position, Vector2 scale, float rotation) : this(position, scale) => Rotation = rotation;
+    
+    public Matrix4x4 ViewMatrix() => CreateScale(Scale.X, Scale.Y, 1f) * CreateRotationZ(Rotation.ToRadians()) * CreateTranslation(Position.X, Position.Y, 0f);
 }
---
+++ N8Engine.Core/Source/Utilities/MathExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace N8Engine.Utilities;
+
+public static class MathExtensions
+{
+    public static float ToRadians(this float degrees) => MathF.PI / 180f * degrees;
+    public static float ToDegrees(this float radians) => 180f / MathF.PI * radians;
+}
