7cbe0decf854c49493e9a393b013799b6ba29c2a
natecurtiss <n8dev.cs@gmail.com>
2022-03-13 12:07:23 -0400

    Refactor PathExtensions

--- N8Engine.Core/Source/Utilities/PathExtensions.cs
+++ N8Engine.Core/Source/Utilities/PathExtensions.cs
@@ -1,17 +1,19 @@
 ﻿using System.IO;
+using System.Runtime.CompilerServices;
 
 namespace N8Engine.Utilities;
 
-public static class PathApproximation
+public static class PathExtensions
 {
-    public static string Find(this string path, int iterations = 8)
+    public static string Find(this string path, [CallerFilePath] string caller = "", int iterations = 8)
     {
+        path = $"{caller}/{path}";
         if (File.Exists(path))
             return path;
         var approximated = path;
         for (var i = 0; i < iterations; i++)
         {
-            approximated = $"../{approximated}";
+            approximated = approximated.Insert(caller.Length, "../");
             if (File.Exists(approximated))
                 return approximated;
         }
---
+++ N8Engine.Tests/Source/PathExtensionsTests.cs
@@ -0,0 +1,15 @@
+﻿using N8Engine.Utilities;
+using NUnit.Framework;
+using static NUnit.Framework.FileAssert;
+
+namespace N8Engine.Tests;
+
+sealed class PathExtensionsTests
+{
+    [Test]
+    public void TestFind()
+    {
+        var path = "Assets/dummy.txt";
+        Exists(path.Find());
+    }
+}
